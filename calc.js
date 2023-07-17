3773f05ac0f6d5d2e19a7a7a7d9f52f349b4538b
unknown <jelishajacob@gmail.com>
2023-07-17 15:02:01 +0530

    calculator

---
+++ calc.js
@@ -0,0 +1,15 @@
+function displayData(n){
+    result.value+=n
+}
+
+function res(){
+    result.value=eval(result.value)
+}
+
+function allClr(){
+    result.value=""
+}
+
+function backSpace(){
+    result.value=(result.value).slice(0,-1)
+}
